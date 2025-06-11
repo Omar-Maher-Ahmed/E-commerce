c9e9caf3b3fb6e77c285a88516dbc2bdc05e5566
Omar-Maher-Ahmed <om8523302@gmail.com>
2025-06-11 15:01:25 +0300

    feat: Comment out admin-only middleware
    This commit temporarily comments out the `adminOnly` middleware.
    This middleware was intended to restrict access to certain routes to users with the "admin" role.
    Commenting it out allows for further development and testing without strict role-based access control.
    The middleware can be re-enabled later by uncommenting the code.

--- src/middelware/auth.middleware.js
+++ src/middelware/auth.middleware.js
@@ -14,4 +14,13 @@ const authMiddleware = (req, res, next) => {
   }
 };
 
+// this middelware for Role Admin only
+// export const adminOnly = (req, res, next) => {
+//   if (req.user.role !== "admin") {
+//     return res.status(403).json({ message: "Forbidden: Admins only" });
+//   }
+//   next();
+// };
+
+
 export default authMiddleware;
