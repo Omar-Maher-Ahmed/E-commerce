572e932fc4bb0782d6e9673679eac5194b3af3da
Omar-Maher-Ahmed <om8523302@gmail.com>
2025-06-11 15:56:12 +0300

    feat: Integrate Joi validation into user registration and login routes
    This commit integrates Joi validation into the user registration and login routes in `src/modules/user/user.router.js`.
    
    - Imported the `validation` middleware and user validation schemas (`userValid`)
    - Applied the `userValid` middleware with respective validation schemas (`registerValidation`, `loginValidation`) to the `/register` and `/login` routes.

--- src/modules/user/user.router.js
+++ src/modules/user/user.router.js
@@ -1,13 +1,14 @@
 import { Router } from "express";
 import * as userController from "./controller/user.controller.js";
 import authMiddleware from "../../middleware/auth.middleware.js";
+import validation from "../../middleware/validation.middleware.js";
+import * as userValid from "./user.validation.js";
 const router = Router();
 
-router.post("/register", userController.register);
-router.post("/login", userController.login);
+router.post("/register",userValid(registerValidation) ,userController.register);
+router.post("/login",userValid(loginValidation) ,userController.login);
 router.get("/profile",authMiddleware, userController.profile);
-router.put("/update",authMiddleware,validation, userController.update);
+router.put("/update",authMiddleware,validation , userController.update);
 router.delete("/delete", userController.deleted);
-
 
 export default router;
